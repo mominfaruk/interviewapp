75de463c83e1b447e5b11b50e9465239c0796962
Al Momin Faruk <31950303+faruk6276@users.noreply.github.com>
2021-06-05 14:24:53 +0600

    Interview CRUD full with ajax

--- interview/urls.py
+++ interview/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
     path('interview/',InterviewView.as_view(),name='interview'),
     path('',interviewlist,name='interviewlist'),
     path('interviewedit/<int:pk>/',interviewEdit.as_view(),name='interviewEdit'),
-    path('interviewdelete/<int:pk>',deleteInterview,name='interview_delete'),
+    path('interviewdelete/<int:pk>/',deleteInterview,name='interview_delete'),
 
     path('sessionlist/',sessionlist,name='session_list'),
     path('addsession/',AddSessionView.as_view(),name='session_add'),
--- interview/views.py
+++ interview/views.py
@@ -3,41 +3,52 @@ from django.views import View
 from .models import *
 from .forms import *
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,JsonResponse,HttpResponse
+from django.template.loader import render_to_string
+from django.template import RequestContext, context
+import json
 # Create your views here.
 
+
 class InterviewView(View):
-    def get(self,request):
-        return render(request, 'interview.html',{'form':InterviewForm})
 
     def post(self,request):
-        my_form=InterviewForm(request.POST)
+        message=[]
+        my_form=InterviewForm(request.POST or None)
         if my_form.is_valid():
-            print('OK')
             title=my_form.cleaned_data['title']
             date=my_form.cleaned_data['date']
             if title=="":
-                messages.info(request, 'Title name shouldn\'t be empty')
+                message.append('Title name shouldn\'t be empty')
             elif date=="":
-                messages.info(request, 'Date name shouldn\'t be empty')
+                message.append('Date name shouldn\'t be empty')
             else:
                 my_form.save()
-                return HttpResponseRedirect('/')
-                
+                message.append('Interview added sucessfully')
+                msg = render_to_string('messages.html', {'messages':message})
+                all_list=InterviewModel.objects.all().order_by('-id')
+                interviewlist=render_to_string('interview/interviewlistTbody.html', {'all_list':all_list})
+                context={'msg':msg,'interviewlist':interviewlist}
+                context=json.dumps(context)
+                return HttpResponse(context,content_type='application/json')
     
 
 
 def interviewlist(request):
-    all_list=InterviewModel.objects.all()
-    return render(request, 'interviewlist.html',{'all_list':all_list})
+    all_list=InterviewModel.objects.all().order_by('-id')
+    return render(request, 'interview/interviewlist.html',{'all_list':all_list,'form':InterviewForm})
 
 class interviewEdit(View):
     def get(self,request,pk):
         inter_view=InterviewModel.objects.get(pk=pk)
         my_form=InterviewForm(instance=inter_view)
-        return render(request, 'interviewedit.html',{'form':my_form})
+        data=render_to_string('interview/interviewedit.html',{'form':my_form,'id':inter_view.id},request=request)
+        #data={'id':inter_view.id,'title':inter_view.title,'date':inter_view.date}
+        #return JsonResponse(data)
+        return HttpResponse(data)
     
     def post(self,request,pk):
+        message=[]
         inter_view=InterviewModel.objects.get(pk=pk)
         my_form=InterviewForm(request.POST,request.FILES,instance=inter_view)
         if my_form.is_valid():
@@ -49,7 +60,12 @@ class interviewEdit(View):
                 messages.info(request, 'Date name shouldn\'t be empty')
             else:
                 my_form.save()
-                return HttpResponseRedirect('/')
+                message.append('Interview edited sucessfully')
+                msg = render_to_string('messages.html', {'messages':message})
+                row=render_to_string('interview/tablerow.html',{'list':inter_view},request=request)
+                context={'msg':msg,'row':row}
+                context=json.dumps(context)
+                return HttpResponse(context,content_type='application/json')
 
 def deleteInterview(request,pk):
     instance = InterviewModel.objects.get(pk=pk)
@@ -94,6 +110,7 @@ class sessionEdit(View):
         if my_form.is_valid():
             title=my_form.cleaned_data['title']
             date=my_form.cleaned_data['date']
+            messages.info(request, 'Title name shouldn\'t be empty')
             if title=="":
                 messages.info(request, 'Title name shouldn\'t be empty')
             elif date=="":
